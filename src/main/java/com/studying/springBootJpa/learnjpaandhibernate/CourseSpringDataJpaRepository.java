ae90d8a43c48b3be18fda60742cf1f74a625d1d9
papachio <john.papachristoss@gmail.com>
2023-02-22 16:26:19 +0200

    Using jpa to communicate with the database.

---
+++ src/main/java/com/studying/springBootJpa/learnjpaandhibernate/CourseSpringDataJpaRepository.java
@@ -0,0 +1,7 @@
+package com.studying.springBootJpa.learnjpaandhibernate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseSpringDataJpaRepository  extends JpaRepository<Course, Long>{
+
+}
